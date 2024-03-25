876e90ec27a7881ddf879401b754b2f861f7ab56
X223MM <86551916+X223MM@users.noreply.github.com>
2024-03-25 13:16:21 +0300

    BuyUpgradeDamage

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,15 +8,11 @@ public class GameManager : MonoBehaviour
 {
    
     private bool gameEnded = false;
-    public int current_money;
-    public Upgrade upgradehero;
+   
     public TextMeshProUGUI moneycheck;
     
 
-    void Start()
-    {
-        
-    }
+   
     void Update()
 
     {
@@ -37,25 +33,8 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0;
     }
 
-    public void BuyUpgrade()
-    {
-        if (current_money >= 10)
-        {
-
-            upgradehero.heroUpgrade();
-            current_money = current_money - 10;
-            moneycheck.text = current_money.ToString();
-            PlayerPrefs.SetInt("Current_money", current_money);
-            PlayerPrefs.Save();
-            Debug.Log(current_money);
-            current_money = PlayerPrefs.GetInt("Current_money", current_money);
-            moneycheck.text = current_money.ToString();
-        }
-        else
-        {
-            Debug.Log("Недосаточно денег");
-        }
+   
 
     }
 
-}
+
