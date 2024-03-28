6059eb436bf10aa43a877e3afcb52e83d793c75e
KelBoy <80912866+KelChel@users.noreply.github.com>
2024-03-28 17:54:04 +0300

    Second Level

--- Assets/Scripts/FrameRateManager.cs
+++ Assets/Scripts/FrameRateManager.cs
@@ -9,10 +9,20 @@ public class FrameRateManager : MonoBehaviour
     float currentFrameTime;
     void Awake()
     {
+        if (PlayerPrefs.GetFloat("fps") == 0f)
+        {
+            TargetFrameRate = 60f;
+        }
+        else
+        {
+            TargetFrameRate = PlayerPrefs.GetFloat("fps");
+        }
+        
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = MaxRate;
         currentFrameTime = Time.realtimeSinceStartup;
         StartCoroutine("WaitForNextFrame");
+        
     }
     IEnumerator WaitForNextFrame()
     {
@@ -27,5 +37,14 @@ public class FrameRateManager : MonoBehaviour
             while (t < currentFrameTime)
                 t = Time.realtimeSinceStartup;
         }
+    }
+
+
+    public void ChangeFPS(float targetfps)
+    {
+        PlayerPrefs.SetFloat("fps", targetfps);
+        TargetFrameRate = PlayerPrefs.GetFloat("fps");
+        StopCoroutine("WaitForNextFrame");
+        StartCoroutine("WaitForNextFrame");
     }
 }
